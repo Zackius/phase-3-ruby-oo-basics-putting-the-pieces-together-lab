e4dfdf5321498e1413b93cb11b47cf60987fcae8
Zacharia <ndunguzachary24@gmail.com>
2022-08-25 16:14:25 +0300

    commit tree 13e0f097640b1573e59a7db436ade98c0e8cb83a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book
+    attr_writer :title
 
+    def initialize(title)
+        @title = title
+end 
+
+attr_accessor :author
+
+def initialize(author)
+    @author = author
+end
+attr_accessor :page_count
+
+def initialize(page_count)
+    @page_count = page_count
+end
+
+attr_accessor :genre 
+
+def initialize(genre)
+    @genre = genre
+end
+def turn_page
+    @@turn_page 
+end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,40 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    attr_accessor :color
+
+    def initialize(color)
+        @color = color
+    end
+
+    attr_accessor :size
+
+    def initialize(size)
+        @size = size
+    end
+    attr_accessor :material
+
+    def initialize(material)
+        @material = material 
+    end
+
+    attr_accessor :condition
+
+    def initialize(condition)
+        @condition = condition 
+    end
+
+def cobble 
+    if @condition  == "old"
+        puts  "Your shoe is as good as new!"
+        end
+
+end
+    
+end
+ 
+
